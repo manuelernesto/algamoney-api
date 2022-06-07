ad6f2708befaef1f54971c1f4cc1e70cefb784e8
manuelernesto <alemdadox@gmail.com>
2022-06-07 22:28:41 +0100

    #migrate the project to Java 17

---
+++ src/main/java/io/github/manuelernesto/money/api/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package io.github.manuelernesto.money.api.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * @author Manuel Ernesto (manuelernest0)
+ * @version 1.0
+ * @date 07/06/22 9:24 PM
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}admin")
+                .roles("ROLE");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf()
+                .disable();
+
+    }
+}
--- src/test/java/io/github/manuelernesto/money/api/AlgaMoneyApiApplicationTests.java
+++ src/test/java/io/github/manuelernesto/money/api/AlgaMoneyApiApplicationTests.java
@@ -1,15 +1,13 @@
 package io.github.manuelernesto.money.api;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.springframework.boot.test.context.SpringBootTest;
+
+
 @SpringBootTest
 public class AlgaMoneyApiApplicationTests {
 
-    @Test
+
     public void contextLoads() {
     }
 
